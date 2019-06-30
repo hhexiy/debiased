7f60c9d80a50e8f3d24ce0163b147fda5e19436d
He He <hhe.xiy@gmail.com>
2019-06-30 01:12:34 +0000

    minor

--- Makefile
+++ Makefile
@@ -41,13 +41,22 @@ train-project-bert:
 	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs $(nepochs) --gpu-id $(gpu) --lr $(lr) --log-interval $(interval) --output-dir output/$(exp) --dropout $(drop) --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type bert --project
 
 train-additive-bert:
-	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs $(nepochs) --gpu-id $(gpu) --lr $(lr) --log-interval $(interval) --output-dir output/$(exp) --dropout $(drop) --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type bert
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 6 --gpu-id $(gpu) --lr 2e-5 --log-interval $(interval) --output-dir output/$(exp) --dropout $(drop) --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type bert
 
 train-additive-da:
-	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 30 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.1 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type da --hidden-size 300
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 80 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.1 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type da --hidden-size 300
 
 train-additive-esim:
-	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 30 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.5 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type esim --hidden-size 300
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 80 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.5 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode all --model-type esim --hidden-size 300
+
+train-remove-bert:
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 6 --gpu-id $(gpu) --lr 2e-5 --log-interval $(interval) --output-dir output/$(exp) --dropout $(drop) --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode last --model-type bert --remove
+
+train-remove-da:
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 80 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.1 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode last --model-type da --hidden-size 300 --remove
+
+train-remove-esim:
+	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 80 --gpu-id $(gpu) --lr 1e-4 --log-interval $(interval) --output-dir output/$(exp) --dropout 0.5 --test-split $(test-split) --additive $(from) --cheat $(cheat_rate) --max-num-examples $(num_ex) --additive-mode last --model-type esim --hidden-size 300 --remove
 
 train-bert-noise:
 	MXNET_GPU_MEM_POOL_TYPE=Round MXNET_HOME=$(mxnet_home) GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --batch-size $(bs) --optimizer bertadam --epochs 4 --gpu-id $(gpu) --lr $(lr) --log-interval $(interval) --output-dir output/$(exp) --dropout 0.1 --test-split $(test-split) --max-num-examples $(num_ex) --noising-by-epoch --word-dropout $(wdrop) --cheat $(cheat_rate) --model-type bert
@@ -57,7 +66,12 @@ test-last:
 	MXNET_GPU_MEM_POOL_TYPE=Round GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --eval-batch-size $(bs) --gpu-id $(gpu) --mode test --init-from $(from) --output-dir eval/$(exp) --test-split $(test-split) --dropout 0.0 --cheat $(cheat_rate) --word-dropout 0 --use-last
 
 test:
-	MXNET_HOME=$(mxnet_home) MXNET_GPU_MEM_POOL_TYPE=Round GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --eval-batch-size 128 --gpu-id $(gpu) --mode test --init-from $(from) --output-dir eval/$(exp) --test-split $(test-split) --dropout 0.0 --cheat $(cheat_rate) --word-dropout 0 --max-num-examples $(num_ex)
+	MXNET_HOME=$(mxnet_home) MXNET_GPU_MEM_POOL_TYPE=Round GLUE_DIR=data/glue_data python -m src.main --task-name $(task) --eval-batch-size 128 --gpu-id $(gpu) --mode test --init-from $(from) --output-dir eval/$(exp) --test-split $(test-split) --dropout 0.0 --cheat $(cheat_rate) --word-dropout 0 --max-num-examples $(num_ex) --additive-mode last
 
 summarize:
 	python scripts/summarize_results.py --runs-dir $(exp)
+
+generate-swap:
+	python scripts/generate_swap_data.py --nli-file data/glue_data/SNLI/original/snli_1.0_test.txt --output data/glue_data/SNLI-swap/test.tsv --format SNLI
+	python scripts/generate_swap_data.py --nli-file data/glue_data/MNLI/dev_matched.tsv --output data/glue_data/MNLI-swap/dev_matched.tsv --format MNLI
+	python scripts/generate_swap_data.py --nli-file data/glue_data/MNLI/dev_mismatched.tsv --output data/glue_data/MNLI-swap/dev_mismatched.tsv --format MNLI
--- scripts/summarize_results.py
+++ scripts/summarize_results.py
@@ -12,7 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--runs-dir', nargs='+')
     parser.add_argument('--output-json')
-    parser.add_argument('--error-analysis', action='store_true')
+    parser.add_argument('--error-analysis', default=None)
     args = parser.parse_args()
     return args
 
@@ -20,18 +20,25 @@ def get_model_config(model_path):
     res = json.load(open('{}/report.json'.format(model_path)))['config']
     return res
 
-def analyze(path):
+def analyze(path, data):
     pred_file = os.path.join('{}/predictions.tsv'.format(os.path.dirname(path)))
     with open(pred_file) as fin:
         reader = csv.DictReader(fin, delimiter='\t')
         preds, labels = [], []
         for row in reader:
-            preds.append('non-entailment' if row['pred'] != 'entailment' else 'entailment')
-            labels.append(row['label'])
+            if data == 'hans':
+                preds.append('non-entailment' if row['pred'] != 'entailment' else 'entailment')
+                labels.append(row['label'])
+            elif data == 'swap':
+                preds.append('non-contradiction' if row['pred'] != 'contradiction' else 'contradiction')
+                labels.append(row['label'])
+            else:
+                preds.append(row['pred'])
+                labels.append(row['label'])
     report = classification_report(labels, preds, output_dict=True)
     return report
 
-def parse_file(path, error_analysis=False):
+def parse_file(path, error_analysis):
     #print('parsing {}'.format(path))
     try:
         res = json.load(open(path))
@@ -53,15 +60,24 @@ def parse_file(path, error_analysis=False):
         additive = len(model_config['additive']) if model_config['additive'] else 0
         last = int(config['use_last'])
         metrics = res['test'][test_split]
-        if test_data.startswith('MNLI-hans'):
+        if test_data.startswith('MNLI-hans') or test_data.startswith('SNLI-swap') or test_data.startswith('MNLI-swap'):
             metric_name = 'mapped-accuracy'
         else:
             metric_name = 'accuracy'
         if additive == 0:
             acc = metrics[metric_name]
             additive = '0'
+            if model_config['superficial'] is True or model_config['superficial'] == 'hypo':
+                model = 'hypo'
+            elif model_config['superficial'] == 'handcrafted':
+                model = 'hand'
         else:
-            acc = metrics['last_{}'.format(metric_name)]
+            if config['additive_mode'] == 'all':
+                acc = metrics['last_{}'.format(metric_name)]
+            elif config['additive_mode'] == 'last':
+                acc = metrics['{}'.format(metric_name)]
+            else:
+                raise ValueError
             prev_models = []
             for prev in model_config['additive']:
                 prev_config = get_model_config(prev)
@@ -81,6 +97,9 @@ def parse_file(path, error_analysis=False):
                 'status': 'failed',
                 'eval_path': path,
                }
+    remove = int(model_config.get('remove', False))
+    if remove == 1:
+        assert not config['remove']
     report = {
             'status': 'success',
             'train_data': train_data,
@@ -91,6 +110,7 @@ def parse_file(path, error_analysis=False):
             'rm_ch': rm_cheat,
             'sup': superficial,
             'add': additive,
+            'rm': remove,
             'wdrop': wdrop,
             'model': model.upper(),
             'acc': acc,
@@ -98,14 +118,22 @@ def parse_file(path, error_analysis=False):
             'eval_path': path,
            }
     constraints = {
-            lambda r: r['mch'] != -1,
-            lambda r: r['tch'] == 0,
+            #lambda r: r['mch'] != -1,
+            #lambda r: r['tch'] == 0,
             #lambda r: r['sup'] == 0,
-            #lambda r: r['add'] in ('0', 'hypo'),
+            lambda r: r['add'] in ('hand', 'hypo', 'cbow', '0'),
+            #lambda r: r['add'] != 'hypo,cbow',
             lambda r: r['wdrop'] in (0,),
+            #lambda r: r['rm'] in (0,),
             #lambda r: r['test_data'].startswith('MNLI-hans'),
-            lambda r: r['test_data'] == 'SNLI-test',
-            #lambda r: r['model'] == 'BERT',
+            #lambda r: r['train_data'].startswith('MNLI'),
+            #lambda r: r['test_data'] == 'SNLI-test',
+            #lambda r: r['test_data'] == 'MNLI-dev_matched',
+            #lambda r: r['train_data'] == 'SNLI',
+            #lambda r: not r['test_data'].endswith('mismatched'),
+            #lambda r: r['model'] in ('BERT', 'DA', 'ESIM'),
+            #lambda r: r['model'] in ('ESIM','HYPO', 'CBOW', 'HAND'),
+            #lambda r: r['model'] in ('HYPO', 'CBOW', 'HAND'),
             }
     for c in constraints:
         if not c(report):
@@ -113,14 +141,31 @@ def parse_file(path, error_analysis=False):
                     'status': 'filtered',
                     'eval_path': path,
                    }
-    if error_analysis:
+    if error_analysis is not None:
         try:
-            acc_report = analyze(path)
-            report.update({
-                'ent': acc_report['entailment']['f1-score'],
-                'n-ent': acc_report['non-entailment']['f1-score'],
-                'acc_report': acc_report,
-                })
+            acc_report = analyze(path, error_analysis)
+            if error_analysis == 'hans':
+                report.update({
+                    'ent': acc_report['entailment']['f1-score'],
+                    'n-ent': acc_report['non-entailment']['f1-score'],
+                    'avg': acc_report['macro avg']['f1-score'],
+                    'acc_report': acc_report,
+                    })
+            elif error_analysis == 'swap':
+                report.update({
+                    'con': acc_report['contradiction']['f1-score'],
+                    'n-con': acc_report['non-contradiction']['f1-score'],
+                    'avg': acc_report['macro avg']['f1-score'],
+                    'acc_report': acc_report,
+                    })
+            else:
+                report.update({
+                    'ent': acc_report['entailment']['f1-score'],
+                    'con': acc_report['contradiction']['f1-score'],
+                    'neu': acc_report['neutral']['f1-score'],
+                    'avg': acc_report['macro avg']['f1-score'],
+                    'acc_report': acc_report,
+                    })
         except Exception as e:
             traceback.print_exc()
             print(os.path.dirname(path))
@@ -159,19 +204,37 @@ def main(args):
 
     columns = [
                ('train_data', 20, 's'),
-               ('test_data', 40, 's'),
-               ('tch', 6, '.1f'),
-               ('mch', 6, '.1f'),
-               ('rm_ch', 6, '.1f'),
-               ('sup', 5, 's'),
-               ('add', 10, 's'),
-               ('wdrop', 10, '.1f'),
+               ('test_data', 50, 's'),
+               #('tch', 6, '.1f'),
+               #('mch', 6, '.1f'),
+               #('rm_ch', 6, '.1f'),
+               #('sup', 5, 's'),
+               ('model', 7, 's'),
+               ('rm', 5, 'd'),
+               ('add', 7, 's'),
+               #('wdrop', 5, '.1f'),
                ('acc', 10, '.3f'),
-               #('ent', 10, '.3f'),
-               #('n-ent', 10, '.3f'),
-               ('model_path', 10, 's'),
-               #('eval-path', 10, 's'),
+               #('model_path', 10, 's'),
               ]
+    if args.error_analysis == 'hans':
+        columns.extend([
+               ('ent', 10, '.3f'),
+               ('n-ent', 10, '.3f'),
+               ('avg', 10, '.3f'),
+            ])
+    elif args.error_analysis == 'swap':
+        columns.extend([
+               ('con', 10, '.3f'),
+               ('n-con', 10, '.3f'),
+            ])
+    elif args.error_analysis is not None :
+        columns.extend([
+               ('ent', 10, '.3f'),
+               ('con', 10, '.3f'),
+               ('neu', 10, '.3f'),
+               ('avg', 10, '.3f'),
+            ])
+    #columns.append(('eval_path', 10, 's'))
     if len(all_res) == 0:
         print('no results found')
         return
@@ -187,13 +250,26 @@ def main(args):
     all_res = sorted(all_res, key=lambda x: [x[c[0]] for c in columns])
 
     #duplicated_paths = []
-    #for i, (r, f) in enumerate(all_res):
-    #    if i > 0 and r == all_res[i-1][0]:
+    #for i, r in enumerate(all_res):
+    #    if i > 0 and r['model_path'] == all_res[i-1]['model_path']:
+    #        f = r['eval_path']
     #        duplicated_paths.append(f)
-    #for f in duplicated_paths:
-    #    print(f)
-    #    shutil.rmtree(os.path.dirname(f))
+    #        print(f)
+    #ans = input('remove duplicated paths? [Y/N]')
+    #if ans == 'Y':
+    #    for f in duplicated_paths:
+    #        shutil.rmtree(os.path.dirname(f))
     #import sys; sys.exit()
+
+    #to_delete = [r['eval_path'] for r in all_res]
+    #for p in to_delete:
+    #    print(p)
+    #ans = input('remove selected paths? [Y/N]')
+    #if ans == 'Y':
+    #    for f in to_delete:
+    #        shutil.rmtree(os.path.dirname(f))
+    #    import sys; sys.exit()
+
 
     print(header)
     for res in all_res:
